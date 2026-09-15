c34f9d96bfb9ada6ee6c0ad0c1c6f97380d59b72
timmaxx <m.n.timchenko@gmail.com>
2026-09-15 23:21:47 +0300

    realize simplest async variant: logs are invisible (ReactiveFirstProject)

--- ReactiveFirstProject/src/main/java/com/example/ReactiveDemo/ReactiveDemoController.java
+++ ReactiveFirstProject/src/main/java/com/example/ReactiveDemo/ReactiveDemoController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+
 @RestController
 public class ReactiveDemoController {
 
@@ -16,9 +18,10 @@ public class ReactiveDemoController {
         return Flux.just("Java", "Guru", "Dot","BY"); //Publisher
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("start");
         new ReactiveDemoController().getItems()
+                .delayElements(Duration.ofMillis(100))
                 .log()
                 .subscribe();
         System.out.println("end");
