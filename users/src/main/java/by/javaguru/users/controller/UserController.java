cd5bea2fca9b9ece8a6617f64442a4c39238ff5d
timmaxx <m.n.timchenko@gmail.com>
2026-09-19 12:48:40 +0300

    Lesson 1.5. Presentation Layer. Implement the `createUser` method with validation and without creating an instance and ResponseStatus is HttpStatus.CREATED. But still synchronous.

--- users/src/main/java/by/javaguru/users/controller/UserController.java
+++ users/src/main/java/by/javaguru/users/controller/UserController.java
@@ -1,6 +1,8 @@
 package by.javaguru.users.controller;
 
 import by.javaguru.users.service.dto.CreateUserDto;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @PostMapping
-    public void createUser(@RequestBody CreateUserDto createUserDto) {
+    //  This status (201) is more appropriate for the successful creation of an entity.
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createUser(@RequestBody @Valid CreateUserDto createUserDto) {
         System.out.println("INFO. Call to the createUser method Either there was no validation (There in no @Valid), or validation was (There in @Valid) and was successful.");
     }
 
--- users/src/main/java/by/javaguru/users/service/dto/CreateUserDto.java
+++ users/src/main/java/by/javaguru/users/service/dto/CreateUserDto.java
@@ -1,5 +1,8 @@
 package by.javaguru.users.service.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -7,9 +10,20 @@ import lombok.Data;
 //  It does not contain an `Id` field, as the ID will be generated upon entity creation.
 public class CreateUserDto {
 
+    @NotBlank(message = "Имя обязательно для заполнения")
+    @Size(min = 2, max = 50, message = "Имя не может быть короче 2 и длиннее 50 символов")
     private String firstName;
+
+    @NotBlank(message = "Фамилия обязательна для заполнения")
+    @Size(min = 2, max = 50, message = "Фамилия не может быть короче 2 и длиннее 50 символов")
     private String lastName;
+
+    @NotBlank(message = "Email не может быть пустым")
+    @Email
     private String email;
+
+    @NotBlank(message = "Пароль не может быть пустым")
+    @Size(min = 8, max = 30, message = "Пароль не может быть короче 8 и длиннее 30 символов")
     private String password;
 
 }
