16f66eff535efcacf5079295a2d9be6284cae5d6
timmaxx <m.n.timchenko@gmail.com>
2026-09-19 14:50:01 +0300

    Lesson 1.5. Presentation Layer. Implement the `createUser` method with validation and without creating an instance and ResponseStatus is HttpStatus.CREATED. It’s already somewhat asynchronous, but still poor.

--- users/src/main/java/by/javaguru/users/controller/UserController.java
+++ users/src/main/java/by/javaguru/users/controller/UserController.java
@@ -4,6 +4,7 @@ import by.javaguru.users.service.dto.CreateUserDto;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/users")
@@ -12,8 +13,9 @@ public class UserController {
     @PostMapping
     //  This status (201) is more appropriate for the successful creation of an entity.
     @ResponseStatus(HttpStatus.CREATED)
-    public void createUser(@RequestBody @Valid CreateUserDto createUserDto) {
+    public void createUser(@RequestBody @Valid Mono<CreateUserDto> createUserDto) {
         System.out.println("INFO. Call to the createUser method Either there was no validation (There in no @Valid), or validation was (There in @Valid) and was successful.");
+        System.out.println("createUserDto = " + createUserDto);
     }
 
 }
